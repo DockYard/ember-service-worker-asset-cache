577005c7fb123c271c8510a73bcb71b798fe0670
Marten Schilstra <marten.schilstra@dockyard.com>
2017-01-25 20:14:05 +0100

    Very Upgrade, much Wow

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var AssetMap = require('./lib/asset-map');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-service-worker-asset-cache',
@@ -10,21 +11,20 @@ module.exports = {
     this._super.included && this._super.included.apply(this, arguments);
     this.app = app;
     this.app.options = this.app.options || {};
-    this.app.options['asset-cache'] = this.app.options['asset-cache'] || {};
+    this.app.options['service-worker-caching'] = this.app.options['service-worker-caching'] || {};
 
-    this.app.options['asset-cache'].appName =
-      this.app.name || this.app.options['asset-cache'].appName;
+    this.app.options['service-worker-caching'].appName =
+        this.app.name || this.app.options['service-worker-caching'].appName;
 
     if (this.app.options.fingerprint && this.app.options.fingerprint.enabled) {
-      this.app.options['asset-cache'].prepend = this.app.options.fingerprint.prepend;
+      this.app.options['service-worker-caching'].prepend = this.app.options.fingerprint.prepend;
     }
   },
 
-  postprocessTree: function(type, tree) {
-    if (type === 'all') {
-      return AssetMap(tree, this.app.options['asset-cache']);
-    }
+  treeForServiceWorker(swTree, appTree) {
+    var options = this.app.options['service-worker-caching'];
+    var assetMapFile = new AssetMap([appTree], options);
 
-    return tree;
+    return mergeTrees([swTree, assetMapFile]);
   }
 };
--- service-worker/index.js
+++ service-worker/index.js
@@ -1,49 +1,62 @@
-import { PROJECT_REVISION, VERSION, addFetchListener } from 'ember-service-worker/service-worker';
+import {
+  files,
+  prepend,
+  version
+} from 'ember-service-worker-caching/service-worker/config';
 
-const CACHE_KEY_PREFIX = 'asset-cache-';
-const CACHE_NAME = `${CACHE_KEY_PREFIX}${PROJECT_REVISION}-${VERSION}`;
+const CACHE_KEY_PREFIX = 'esw-caching';
+const CACHE_NAME = `${CACHE_KEY_PREFIX}-${version}`;
+const CACHE_URLS = files.map((file) => {
+  return new URL(file, (prepend || self.location)).toString();
+});
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        cache.add(self.registration.scope);
-        return fetch('files.json').then(function(response) {
-          return response.json();
-        }).then(function(json) {
-          var files = json.files.map(function(file) {
-            return json.prepend ? json.prepend + file : file;
-          }).map(function(path) {
-            return new Request(path, { mode: 'no-cors' });
-          });
-
-          return cache.addAll(files);
-        }).catch(function() {
-        });
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return Promise.all(CACHE_URLS.map((url) => {
+          return cache.add(new Request(url, {
+            credentials: 'include'
+          }));
+        }));
       })
   );
 });
 
-addFetchListener(function(event) {
-  return caches
-    .match(event.request, { cacheName: CACHE_NAME })
-    .then(function(response) {
-      if (response) {
-        return response;
-      } else if (event.request.headers.get('accept').indexOf('text/html') >= 0) {
-        return caches.match(self.registration.scope);
-      }
-    });
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then((cacheNames) => {
+        cacheNames.forEach((cacheName) => {
+          if (cacheName.indexOf(CACHE_KEY_PREFIX) === 0 && cacheName !== CACHE_NAME) {
+            caches.delete(cacheName);
+          }
+        });
+      }),
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.keys().then((keys) => {
+          keys.forEach((request) => {
+            if (CACHE_URLS.indexOf(request.url) === -1) {
+              cache.delete(request);
+            }
+          });
+        });
+      })
+    ])
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      cacheNames.forEach(function(cacheName) {
-        if (cacheName.indexOf(CACHE_KEY_PREFIX) === 0 && cacheName !== CACHE_NAME) {
-          caches.delete(cacheName);
-        }
-      });
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET' && CACHE_URLS.indexOf(event.request.url) !== -1) {
+    event.respondWith(
+      caches
+        .match(event.request, { cacheName: CACHE_NAME })
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+        })
+    );
+  }
 });
