d5c5169cecec568308d6286295f1501d17d05fac
Marten Schilstra <marten.schilstra@dockyard.com>
2017-01-26 16:22:31 +0100

    Clean up/refactor SW code

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,14 +1,39 @@
 import {
-  files,
-  prepend,
-  version
+  FILES,
+  PREPEND,
+  VERSION
 } from 'ember-service-worker-asset-cache/service-worker/config';
 
 const CACHE_KEY_PREFIX = 'esw-asset-cache';
-const CACHE_NAME = `${CACHE_KEY_PREFIX}-${version}`;
-const CACHE_URLS = files.map((file) => {
-  return new URL(file, (prepend || self.location)).toString();
+const CACHE_NAME = `${CACHE_KEY_PREFIX}-${VERSION}`;
+const CACHE_URLS = FILES.map((file) => {
+  return new URL(file, (PREPEND || self.location)).toString();
 });
+
+const DELETE_STALE_CACHES = () => {
+  return caches.keys().then((cacheNames) => {
+    cacheNames.forEach((cacheName) => {
+      let isAssetCache = cacheName.indexOf(CACHE_KEY_PREFIX) === 0;
+      let isNotCurrentCache = cacheName !== CACHE_NAME;
+
+      if (isNotCurrentCache && isNotCurrentCache) {
+        caches.delete(cacheName);
+      }
+    });
+  });
+};
+
+const PRUNE_CURRENT_CACHE = () => {
+  caches.open(CACHE_NAME).then((cache) => {
+    return cache.keys().then((keys) => {
+      keys.forEach((request) => {
+        if (CACHE_URLS.indexOf(request.url) === -1) {
+          cache.delete(request);
+        }
+      });
+    });
+  });
+};
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -21,36 +46,19 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     Promise.all([
-      caches.keys().then((cacheNames) => {
-        cacheNames.forEach((cacheName) => {
-          if (cacheName.indexOf(CACHE_KEY_PREFIX) === 0 && cacheName !== CACHE_NAME) {
-            caches.delete(cacheName);
-          }
-        });
-      }),
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.keys().then((keys) => {
-          keys.forEach((request) => {
-            if (CACHE_URLS.indexOf(request.url) === -1) {
-              cache.delete(request);
-            }
-          });
-        });
-      })
+      DELETE_STALE_CACHES(),
+      PRUNE_CURRENT_CACHE()
     ])
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.method === 'GET' && CACHE_URLS.indexOf(event.request.url) !== -1) {
+  let isGETRequest = event.request.method === 'GET';
+  let shouldRespond = CACHE_URLS.indexOf(event.request.url) !== -1;
+
+  if (isGETRequest && shouldRespond) {
     event.respondWith(
-      caches
-        .match(event.request, { cacheName: CACHE_NAME })
-        .then((response) => {
-          if (response) {
-            return response;
-          }
-        })
+      caches.match(event.request, { cacheName: CACHE_NAME })
     );
   }
 });
