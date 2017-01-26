938ddb9576f6347c425d43aea0482ce558054302
Marten Schilstra <marten.schilstra@dockyard.com>
2017-01-26 14:07:16 +0100

    Fix config prop name

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ module.exports = {
     this._super.included && this._super.included.apply(this, arguments);
     this.app = app;
     this.app.options = this.app.options || {};
-    this.app.options['service-worker-caching'] = this.app.options['service-worker-caching'] || {};
+    this.app.options['asset-cache'] = this.app.options['asset-cache'] || {};
 
-    this.app.options['service-worker-caching'].appName =
-        this.app.name || this.app.options['service-worker-caching'].appName;
+    this.app.options['asset-cache'].appName =
+        this.app.name || this.app.options['asset-cache'].appName;
 
     if (this.app.options.fingerprint && this.app.options.fingerprint.enabled) {
-      this.app.options['service-worker-caching'].prepend = this.app.options.fingerprint.prepend;
+      this.app.options['asset-cache'].prepend = this.app.options.fingerprint.prepend;
     }
   },
 
   treeForServiceWorker(swTree, appTree) {
-    var options = this.app.options['service-worker-caching'];
+    var options = this.app.options['asset-cache'];
     var assetMapFile = new AssetMap([appTree], options);
 
     return mergeTrees([swTree, assetMapFile]);
